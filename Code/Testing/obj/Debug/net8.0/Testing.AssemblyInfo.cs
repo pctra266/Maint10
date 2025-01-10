686a2a88d97c7d9a8739861a08b49a06c1700378
Tra Pham <phamtra001@gmail.com>
2025-01-10 08:42:21 +0700

    change Name project

--- Code/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ Code/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc15799fa197af19f991ff7596f44ecdf1b06dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2556e5c39a977015ae48494ad28a162b38ade6a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
