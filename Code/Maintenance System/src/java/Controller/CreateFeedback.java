a1d6be3fd3190c7434953549741434272e4e0bbe
Tra Pham <phamtra001@gmail.com>
2025-01-18 07:52:54 +0700

    udpate template

--- Code/Maintenance System/src/java/Controller/CreateFeedback.java
+++ Code/Maintenance System/src/java/Controller/CreateFeedback.java
@@ -56,7 +56,7 @@ public class CreateFeedback extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        response.sendRedirect("CreateFeedback.jsp");
     } 
 
     /** 
