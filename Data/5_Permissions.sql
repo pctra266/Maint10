f2a4b8e836182f162db645b047f90113693dc9cd
LilyStoner <hainam25072004@gmail.com>
2025-03-29 13:35:33 +0700

    Update 5_Permissions.sql

--- Data/5_Permissions.sql
+++ Data/5_Permissions.sql
@@ -1,9 +1,15 @@
 ﻿USE MaintainManagement
+
+DELETE FROM Permissions;
+DBCC CHECKIDENT ('Permissions', RESEED, 0);
+DELETE FROM Role_Permissions
+
+
 INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
 ('VIEW_COMPONENTS_WAREHOUSE', '/ComponentWarehouse'),
 ('DELETE_COMPONENT', '/ComponentWarehouse/Delete'),
-('VIEW_WARRANTY_CARD_LIST', '/Warehouse/WarrantyCard '),
-('ADD_NEW_COMPONENT', '/ComponentWarehouse/Add '),
+('VIEW_WARRANTY_CARD_LIST', '/Warehouse/WarrantyCard'),
+('ADD_NEW_COMPONENT', '/ComponentWarehouse/Add'),
 ('SEARCH_ADVANCE_COMPONENT', '/ComponentWarehouse/Search'),
 ('VIEW_COMPONENT_DETAIL', '/ComponentWarehouse/Detail'),
 ('EDIT_COMPONENT', '/ComponentWarehouse/Edit'),
@@ -12,7 +18,7 @@ INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
 ('EXPORT_COMPONENTS', '/ExportComponents'),
 ('IMPORT_COMPONENTS', '/ImportComponents'),
 ('VIEW_WARRANTY_CARD', '/WarrantyCard'),
-('ADD_WARRANTY_CARD', '/WarrantyCard/Add '),
+('ADD_WARRANTY_CARD', '/WarrantyCard/Add'),
 ('DELETE_WARRANTY_CARD', '/WarrantyCard/Delete'),
 ('VIEW_WARRANTY_CARD_DETAIL', '/WarrantyCard/Detail'),
 ('PROCESSING_WARRANTY_CARD_INCOM', '/WarrantyCard/Detail?action=process'),
@@ -45,6 +51,7 @@ INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
 ('CHANGE_PASSWORD_FORM_JSP', '/ChangePasswordForm.jsp'),
 ('CHANGE_PASSWORD', '/changepassword'),
 ('VIEW_PROFILE', '/profile'),
+('UPDATE_PROFILE', '/profile'),
 ('VIEW_FEEDBACK', '/feedback?action=viewFeedback'),
 ('DELETE_FEEDBACK', '/feedback?action=deleteFeedback'),
 ('UPDATE_FEEDBACK', '/feedback?action=updateFeedback'),
@@ -100,7 +107,7 @@ INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
 ('BANK_PAY', '/vnpayajax'),
 ('Pay_RETURN', '/Payment/Return'),
 ('ADD_WARRANTY_CARD_2', '/WarrantyCard/Add?action=create'),
-('ADD_WARRANTY_CARD_3', '/WarrantyCard/Add?action=receive '),
+('ADD_WARRANTY_CARD_3', '/WarrantyCard/Add?action=receive'),
 ('DELETE_COMPONENT_IN_REPAIR_LIST', '/WarrantyCard/Detail?action=delete'),
 ('CREATE_CONTRACTOR_CARD_2', '/WarrantyCard/OutsourceRequest'),
 ('WARRANTY_CARD_OUTSOURCE_PROCESS', '/WarrantyCard/OutsourceRequest?action=processOutsource'),
@@ -119,10 +126,8 @@ INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
 ('VIEW_DETAIL_PRODUCT', '/viewProduct?action=update'),
 ('PAY_FOR_OUTSOURCE', '/Invoice/PayOutsource'),
 ('EXPORT_INVOICE_PDF_REPAIR', '/exportInvoicePDF'),
-('VIEW_DETAIL_WARRANTYCARD_CONTRACTOR', '/warrantyCardDetailContractor')
-
-
-
+('VIEW_DETAIL_WARRANTYCARD_CONTRACTOR', '/warrantyCardDetailContractor'),
+('PERMISSION ', '/permissions')
 
 
 INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
@@ -134,7 +139,7 @@ INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
 (1,6),(2,6),(3,6),
 (1,7),(3,7),
 (1,8),(3,8),
-(1,9),(3,9),
+(1,9),(2,9),(3,9),
 (1,10),(3,10),
 (1,11),(3,11),
 (1,12),(2,12),(5,12),
@@ -171,10 +176,10 @@ INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
 (1,43),(2,43),(3,43),(4,43),(5,43),
 (1,44),(2,44),(3,44),(4,44),(5,44),
 (1,45),(2,45),(3,45),(4,45),(5,45),
-(1,46),(5,46),
+(1,46),(2,46),(3,46),(4,46),(5,46),
 (1,47),(5,47),
 (1,48),(5,48),
-(1,49),
+(1,49),(5,49),
 (1,50),
 (1,51),
 (1,52),
@@ -192,49 +197,49 @@ INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
 (1,64),
 (1,65),
 (1,66),
-(1,67),(2,67),(3,67),(4,67),(5,67),
-(1,68),(2,68),
+(1,67),
+(1,68),(2,68),(3,68),(4,68),(5,68),
 (1,69),(2,69),
 (1,70),(2,70),
 (1,71),(2,71),
-(1,72),(3,72),
-(1,73),(2,73),
+(1,72),(2,72),
+(1,73),(3,73),
 (1,74),(2,74),
 (1,75),(2,75),
 (1,76),(2,76),
 (1,77),(2,77),
 (1,78),(2,78),
-(1,79),(3,79),
+(1,79),(2,79),
 (1,80),(3,80),
 (1,81),(3,81),
 (1,82),(3,82),
-(4,83),
+(1,83),(3,83),
 (4,84),
 (4,85),
 (4,86),
 (4,87),
-(1,88),(5,88),
+(4,88),
 (1,89),(5,89),
 (1,90),(5,90),
 (1,91),(5,91),
 (1,92),(5,92),
-(1,93),(2,93),(3,93),(4,93),(5,93),
-(1,94),
-(1,95),(3,95),
-(1,96),(2,96),(3,96),(4,96),(5,96),
+(1,93),(5,93),
+(1,94),(2,94),(3,94),(4,94),(5,94),
+(1,95),
+(1,96),(3,96),
 (1,97),(2,97),(3,97),(4,97),(5,97),
-(2,98),
+(1,98),(2,98),(3,98),(4,98),(5,98),
 (2,99),
 (2,100),
 (2,101),
 (2,102),
 (2,103),
 (2,104),
-(2,105),(3,105),
-(1,106),(2,106),
+(2,105),
+(2,106),(3,106),
 (1,107),(2,107),
 (1,108),(2,108),
-(1,109),(3,109),
+(1,109),(2,109),
 (1,110),(3,110),
 (1,111),(3,111),
 (1,112),(3,112),
@@ -243,18 +248,11 @@ INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
 (1,115),(3,115),
 (1,116),(3,116),
 (1,117),(3,117),
-(1,118),(2,118),
+(1,118),(2,118),(3,118),
+(1,119),(2,119),
 (4,120),
-(4,121)
+(4,121),
+(1,122)
 
 
 
-INSERT INTO [dbo].[Permissions] ([PermissionName], [Link]) VALUES
-('VIEW_DETAIL_PRODUCT', '/viewProduct?action=update')
-INSERT INTO [dbo].[Role_Permissions] ([RoleID], [PermissionID] ) VALUES
-(1,117),(2,117),(3,117),(5,117) 
-
-
---DELETE FROM Permissions;
---DBCC CHECKIDENT ('Permissions', RESEED, 0);
---DELETE FROM Role_Permissions
